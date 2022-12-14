34b498b9fd0880bbcff38d3fd145233d0d239b36
seunghyubchoi <seunghyub.choi@gmail.com>
2022-12-14 14:44:47 +0900

    2221214

---
+++ 99_JDBC_HOMEWORK/src/test/TestRun.java
@@ -0,0 +1,211 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class TestRun {
+
+	public static void main(String[] args) {
+
+		while(true) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("1. SELECT");
+		System.out.println("2. INSERT");
+		System.out.println("3. DELETE");
+		System.out.println("4. 종료");
+		System.out.print("메뉴 입력 : ");
+		
+		
+		
+		
+		
+		
+		int menu = sc.nextInt();
+		sc.nextLine();
+		
+			switch (menu) {
+			case 1:
+				select();
+				break;
+
+			case 2:
+				insert();
+				break;
+
+			case 3:
+				delete();
+				break;
+
+			case 4:
+				System.out.println("바이바이");
+				return;
+
+			default:
+
+			}
+		}
+	}
+	 
+	// select 구문 
+	public static void select() {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		DecimalFormat decFormat = new DecimalFormat("###,###");
+		
+		
+		String sql = "SELECT * FROM PRODUCT";
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","JDBC","JDBC");
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(sql);
+
+			while(rset.next()) {
+				int pno = rset.getInt("PNO");
+				String pname = rset.getString("PNAME");
+				int price = rset.getInt("PRICE");
+				String newPrice = decFormat.format(price);
+				//String lastPrice = String.format("$%,d", newPrice);
+				Date pdate = rset.getDate("REG_DATE");
+				
+				System.out.println(pno + ", " + pname + ", " + newPrice + ", " + pdate);
+
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rset.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	
+	}
+	
+	
+	// insert 구문
+	public static void insert() {
+		Scanner sc = new Scanner(System.in);
+		
+		//System.out.print("PNO : ");
+		//int pno = sc.nextInt();
+
+		System.out.print("PNAME : ");
+		
+		String pname = sc.nextLine();
+		System.out.print("PRICE : ");
+		int price = sc.nextInt();
+		
+		sc.nextLine();
+		
+		
+		int result = 0;
+		Connection conn = null;
+		Statement stmt = null;
+		String sql = "INSERT INTO PRODUCT VALUES(SEQ_PNO.NEXTVAL, '" + pname + "'," + price + ", SYSDATE)";
+
+		// jdbc driver 등록
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// Connection 객체 생성
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+
+			// Statement 객체 생성
+			stmt = conn.createStatement();
+
+			// sql문 전달
+			result = stmt.executeUpdate(sql);
+
+			// 트랜젝션 처리
+			if (result > 0) {
+				conn.commit();
+			} else {
+				conn.rollback();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (result > 0) {
+			System.out.println(pname +"(이)가 추가 되었습니다");
+		} else {
+			System.out.println("insert 실패");
+		}
+	}
+	
+	// DELETE 구문
+	
+	public static void delete() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이름을 입력하세요 : ");
+		String pname = sc.nextLine();
+		
+		String sql = "DELETE FROM PRODUCT WHERE PNAME = '" + pname + "'";
+		
+		int result = 0;
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// Connection 객체 생성
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+
+			// Statement 객체 생성
+			stmt = conn.createStatement();
+
+			// sql문 전달
+			result = stmt.executeUpdate(sql);
+
+			// 트랜젝션 처리
+			if (result > 0) {
+				conn.commit();
+			} else {
+				conn.rollback();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (result > 0) {
+			System.out.println("delete 성공");
+		} else {
+			System.out.println("delete 실패");
+		}
+	}
+
+}
---
+++ Sample Script.sql
@@ -0,0 +1,100 @@
+-- 깃허브 클론하기
+-- JDBC 계정 만들고 권한 부여하기
+
+-- 테이블 삭제
+DROP TABLE MEMBER; -- 뭘 만들면 항상 맨 위에 DROP ~ 쓰기
+
+-- 시퀀스 삭제
+DROP SEQUENCE SEQ_USERNO;
+
+
+CREATE TABLE MEMBER(
+    USERNO NUMBER PRIMARY KEY,                              -- 회원번호
+    USERID VARCHAR2(15) NOT NULL UNIQUE,                    -- 회원아이디
+    USERPWD VARCHAR2(15) NOT NULL,                          -- 회원비밀번호
+    USERNAME VARCHAR2(20) NOT NULL,                         -- 회원명
+    GENDER CHAR(1) CHECK(GENDER IN ('M','F')),              -- 성별
+    AGE NUMBER,                                             -- 나이
+    EMAIL VARCHAR2(30),                                     -- 이메일
+    PHONE CHAR(11),                                         -- 전화번호
+    ADDRESS VARCHAR2(100),                                  -- 주소
+    HOBBY VARCHAR2(50),                                     -- 취미
+    ENROLLDATE DATE DEFAULT SYSDATE NOT NULL                -- 가입일
+);
+
+
+CREATE SEQUENCE SEQ_USERNO
+NOCACHE;
+
+
+INSERT INTO MEMBER 
+VALUES(SEQ_USERNO.NEXTVAL, 'admin', '1234', '관리자', 'M', 45, 'admin@iei.or.kr', '01012345555','서울',NULL,'2021-07-27');
+
+INSERT INTO MEMBER 
+VALUES(SEQ_USERNO.NEXTVAL, 'user01', 'pass01', '홍길동', null, 23, 'user01@iei.or.kr', '01055558888','부산', '등산,영화보기','2021-08-02');
+
+COMMIT;
+
+CREATE TABLE TEST(
+    TNO NUMBER,
+    TNAME VARCHAR2(20),
+    TDATE DATE
+);
+
+-- 테이블 DROP 제거
+DROP TABLE PRODUCT; 
+
+-- 시퀀스 SEQ_PNO 제거
+DROP SEQUENCE SEQ_PNO;
+
+CREATE TABLE PRODUCT(
+    PNO NUMBER PRIMARY KEY,
+    PNAME VARCHAR2(30) NOT NULL,
+    PRICE NUMBER NOT NULL,
+    REG_DATE DATE DEFAULT SYSDATE NOT NULL 
+);
+
+CREATE SEQUENCE SEQ_PNO
+NOCACHE;
+
+COMMIT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
